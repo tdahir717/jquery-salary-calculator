7e470cea0f0be1f705e41a61867898f322e85e8e
Tayasir Dahir <tdahir717@gmail.com>
2020-05-25 07:12:12 -0500

    fixed sourcing

---
+++ scripts/client.js
@@ -0,0 +1,33 @@
+$(document).ready(onReady);
+
+//onReady
+
+function onReady(){
+    console.log('in JQ');
+    
+}
+
+
+//adding employee logic
+
+let employeeData = [];
+
+function addEmployee(){
+    console.log('in addEmployee');
+    
+}
+
+
+
+
+
+
+
+
+//calculating math logic
+
+
+
+//appending
+
+
