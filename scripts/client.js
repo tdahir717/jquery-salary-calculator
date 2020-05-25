68b08e12b054fe340d38b6e5042937287840da11
Tayasir Dahir <tdahir717@gmail.com>
2020-05-25 07:44:48 -0500

    Math logic done. Everything working up to this point

--- scripts/client.js
+++ scripts/client.js
@@ -2,11 +2,11 @@ $(document).ready(onReady);
 
 //onReady
 
-function onReady(){
+function onReady() {
     // console.log('in JQ');
     $(`#addEmployeesBtn`).on(`click`, addEmployee);
     $(`#addEmployeesBtn`).on(`click`, appendData);
-    
+    $(`#addEmployeesBtn`).on(`click`, calculateMonthlySalary);
 }
 
 
@@ -14,7 +14,7 @@ function onReady(){
 
 let employeeData = [];
 
-function addEmployee(){
+function addEmployee() {
     // console.log('in addEmployee');\
 
     let employeeToAdd = {
@@ -25,21 +25,29 @@ function addEmployee(){
         annualSalary: $(`#annualSalaryIn`).val()
     }
     console.log('employeeToAdd', employeeToAdd);
-    
+
     //push into array
     employeeData.push(employeeToAdd);
-    
+
 }// end appData
 
 //calculating math logic
-
-
+function calculateMonthlySalary() {
+    let monthlyCost = 0;
+    let el = $(`#totalMonthlySalary`);
+    for (let i = 0; i < employeeData.length; i++) {
+        monthlyCost += Number(Math.round(((`${employeeData[i].annualSalary}`) / 12) + 'e2') + 'e-2');
+        // Number(Math.round(number + 'e2') + 'e-2'); found on the stackOverflowt. Round 2 decimal spots.
+        el.empty();
+        el.append(monthlyCost);
+    }
+}
 
 //appending
 function appendData() {
     let el = $(`tbody`)
     el.empty();
-    for (let i=0; i < employeeData.length; i++){
+    for (let i = 0; i < employeeData.length; i++) {
         el.append(
             `<tr>
                 <td>${employeeData[i].firstName}</td>
@@ -49,11 +57,11 @@ function appendData() {
                 <td>${employeeData[i].annualSalary}</td>
             </tr>`
         )
-        $(`#firstNameIn`).val(''),
-        $(`#lastNameIn`).val(''),
-        $(`#idNumberIn`).val(''),
-        $(`#jobTitleIn`).val(''),
-        $(`#annualSalaryIn`).val('')
+       
+            $(`#firstNameIn`).val(''),
+            $(`#lastNameIn`).val(''),
+            $(`#idNumberIn`).val(''),
+            $(`#jobTitleIn`).val(''),
+            $(`#annualSalaryIn`).val('')
     }
-}
-
+}// end append
